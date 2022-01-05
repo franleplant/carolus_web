f14507ab1e7f91d777ee928801f289ecd76c0121
Fran Guijarro <franleplant@gmail.com>
2022-01-05 20:20:58 -0300

    supported chains

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -10,17 +10,17 @@ import { ARBITRUM_LIST, OPTIMISM_LIST } from "./lists";
  *  * List of all the networks supported by the Uniswap Interface
  *   */
 export enum SupportedChainId {
-  MAINNET = 1,
-  ROPSTEN = 3,
-  RINKEBY = 4,
-  GOERLI = 5,
-  KOVAN = 42,
+  //MAINNET = 1,
+  //ROPSTEN = 3,
+  //RINKEBY = 4,
+  //GOERLI = 5,
+  //KOVAN = 42,
 
-  ARBITRUM_ONE = 42161,
-  ARBITRUM_RINKEBY = 421611,
+  //ARBITRUM_ONE = 42161,
+  //ARBITRUM_RINKEBY = 421611,
 
-  OPTIMISM = 10,
-  OPTIMISTIC_KOVAN = 69,
+  //OPTIMISM = 10,
+  //OPTIMISTIC_KOVAN = 69,
 
   POLYGON = 137,
   POLYGON_MUMBAI = 80001,
@@ -45,11 +45,11 @@ export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(
  *  * All the chain IDs that are running the Ethereum protocol.
  *   */
 export const L1_CHAIN_IDS = [
-  SupportedChainId.MAINNET,
-  SupportedChainId.ROPSTEN,
-  SupportedChainId.RINKEBY,
-  SupportedChainId.GOERLI,
-  SupportedChainId.KOVAN,
+  //SupportedChainId.MAINNET,
+  //SupportedChainId.ROPSTEN,
+  //SupportedChainId.RINKEBY,
+  //SupportedChainId.GOERLI,
+  //SupportedChainId.KOVAN,
   SupportedChainId.POLYGON,
   SupportedChainId.POLYGON_MUMBAI,
 ] as const;
@@ -61,10 +61,10 @@ export type SupportedL1ChainId = typeof L1_CHAIN_IDS[number];
  *   * The expectation is that all of these networks have immediate transaction confirmation.
  *    */
 export const L2_CHAIN_IDS = [
-  SupportedChainId.ARBITRUM_ONE,
-  SupportedChainId.ARBITRUM_RINKEBY,
-  SupportedChainId.OPTIMISM,
-  SupportedChainId.OPTIMISTIC_KOVAN,
+  //SupportedChainId.ARBITRUM_ONE,
+  //SupportedChainId.ARBITRUM_RINKEBY,
+  //SupportedChainId.OPTIMISM,
+  //SupportedChainId.OPTIMISTIC_KOVAN,
 ] as const;
 
 export type SupportedL2ChainId = typeof L2_CHAIN_IDS[number];
@@ -73,15 +73,15 @@ export type SupportedL2ChainId = typeof L2_CHAIN_IDS[number];
  *  * These are the network URLs used by the interface when there is not another available source of chain data
  *   */
 export const INFURA_NETWORK_URLS: { [key in SupportedChainId]: string } = {
-  [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
+  //[SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.POLYGON]: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.POLYGON_MUMBAI]: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.HARDHAT_NODE]: `https://http://127.0.0.1:8545`,
@@ -135,144 +135,144 @@ export type ChainInfoMap = {
 } & { readonly [chainId in SupportedL1ChainId]: L1ChainInfo };
 
 export const CHAIN_INFO: ChainInfoMap = {
-  [SupportedChainId.MAINNET]: {
-    networkType: NetworkType.L1,
-    docs: "https://docs.uniswap.org/",
-    explorer: "https://etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/",
-    label: "Ethereum",
-    logoUrl: ethereumLogoUrl,
-    addNetworkInfo: {
-      nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-      rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.MAINNET],
-    },
-  },
-  [SupportedChainId.RINKEBY]: {
-    networkType: NetworkType.L1,
-    docs: "https://docs.uniswap.org/",
-    explorer: "https://rinkeby.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/",
-    label: "Rinkeby",
-    logoUrl: ethereumLogoUrl,
-    addNetworkInfo: {
-      nativeCurrency: { name: "Rinkeby Ether", symbol: "rETH", decimals: 18 },
-      rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.RINKEBY],
-    },
-  },
-  [SupportedChainId.ROPSTEN]: {
-    networkType: NetworkType.L1,
-    docs: "https://docs.uniswap.org/",
-    explorer: "https://ropsten.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/",
-    label: "Ropsten",
-    logoUrl: ethereumLogoUrl,
-    addNetworkInfo: {
-      nativeCurrency: { name: "Ropsten Ether", symbol: "ropETH", decimals: 18 },
-      rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.ROPSTEN],
-    },
-  },
-  [SupportedChainId.KOVAN]: {
-    networkType: NetworkType.L1,
-    docs: "https://docs.uniswap.org/",
-    explorer: "https://kovan.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/",
-    label: "Kovan",
-    logoUrl: ethereumLogoUrl,
-    addNetworkInfo: {
-      nativeCurrency: { name: "Kovan Ether", symbol: "kovETH", decimals: 18 },
-      rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.KOVAN],
-    },
-  },
-  [SupportedChainId.GOERLI]: {
-    networkType: NetworkType.L1,
-    docs: "https://docs.uniswap.org/",
-    explorer: "https://goerli.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/",
-    label: "Görli",
-    logoUrl: ethereumLogoUrl,
-    addNetworkInfo: {
-      nativeCurrency: { name: "Görli Ether", symbol: "görETH", decimals: 18 },
-      rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.GOERLI],
-    },
-  },
-  [SupportedChainId.OPTIMISM]: {
-    networkType: NetworkType.L2,
-    blockWaitMsBeforeWarning: ms("25m"),
-    bridge: "https://gateway.optimism.io/?chainId=1",
-    defaultListUrl: OPTIMISM_LIST,
-    docs: "https://optimism.io/",
-    explorer: "https://optimistic.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/optimism/",
-    label: "Optimism",
-    logoUrl: optimismLogoUrl,
-    statusPage: "https://optimism.io/status",
-    helpCenterUrl:
-      "https://help.uniswap.org/en/collections/3137778-uniswap-on-optimistic-ethereum-oξ",
-    addNetworkInfo: {
-      nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-      rpcUrl: "https://mainnet.optimism.io",
-    },
-  },
-  [SupportedChainId.OPTIMISTIC_KOVAN]: {
-    networkType: NetworkType.L2,
-    blockWaitMsBeforeWarning: ms("25m"),
-    bridge: "https://gateway.optimism.io/",
-    defaultListUrl: OPTIMISM_LIST,
-    docs: "https://optimism.io/",
-    explorer: "https://optimistic.etherscan.io/",
-    infoLink: "https://info.uniswap.org/#/optimism/",
-    label: "Optimistic Kovan",
-    logoUrl: optimismLogoUrl,
-    statusPage: "https://optimism.io/status",
-    helpCenterUrl:
-      "https://help.uniswap.org/en/collections/3137778-uniswap-on-optimistic-ethereum-oξ",
-    addNetworkInfo: {
-      nativeCurrency: {
-        name: "Optimistic Kovan Ether",
-        symbol: "kovOpETH",
-        decimals: 18,
-      },
-      rpcUrl: "https://kovan.optimism.io",
-    },
-  },
-  [SupportedChainId.ARBITRUM_ONE]: {
-    networkType: NetworkType.L2,
-    blockWaitMsBeforeWarning: ms("10m"),
-    bridge: "https://bridge.arbitrum.io/",
-    docs: "https://offchainlabs.com/",
-    explorer: "https://arbiscan.io/",
-    infoLink: "https://info.uniswap.org/#/arbitrum",
-    label: "Arbitrum",
-    logoUrl: arbitrumLogoUrl,
-    defaultListUrl: ARBITRUM_LIST,
-    helpCenterUrl:
-      "https://help.uniswap.org/en/collections/3137787-uniswap-on-arbitrum",
-    addNetworkInfo: {
-      nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-      rpcUrl: "https://arb1.arbitrum.io/rpc",
-    },
-  },
-  [SupportedChainId.ARBITRUM_RINKEBY]: {
-    networkType: NetworkType.L2,
-    blockWaitMsBeforeWarning: ms("10m"),
-    bridge: "https://bridge.arbitrum.io/",
-    docs: "https://offchainlabs.com/",
-    explorer: "https://rinkeby-explorer.arbitrum.io/",
-    infoLink: "https://info.uniswap.org/#/arbitrum/",
-    label: "Arbitrum Rinkeby",
-    logoUrl: arbitrumLogoUrl,
-    defaultListUrl: ARBITRUM_LIST,
-    helpCenterUrl:
-      "https://help.uniswap.org/en/collections/3137787-uniswap-on-arbitrum",
-    addNetworkInfo: {
-      nativeCurrency: {
-        name: "Rinkeby Arbitrum Ether",
-        symbol: "rinkArbETH",
-        decimals: 18,
-      },
-      rpcUrl: "https://rinkeby.arbitrum.io/rpc",
-    },
-  },
+  //[SupportedChainId.MAINNET]: {
+  //networkType: NetworkType.L1,
+  //docs: "https://docs.uniswap.org/",
+  //explorer: "https://etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/",
+  //label: "Ethereum",
+  //logoUrl: ethereumLogoUrl,
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  //rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.MAINNET],
+  //},
+  //},
+  //[SupportedChainId.RINKEBY]: {
+  //networkType: NetworkType.L1,
+  //docs: "https://docs.uniswap.org/",
+  //explorer: "https://rinkeby.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/",
+  //label: "Rinkeby",
+  //logoUrl: ethereumLogoUrl,
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Rinkeby Ether", symbol: "rETH", decimals: 18 },
+  //rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.RINKEBY],
+  //},
+  //},
+  //[SupportedChainId.ROPSTEN]: {
+  //networkType: NetworkType.L1,
+  //docs: "https://docs.uniswap.org/",
+  //explorer: "https://ropsten.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/",
+  //label: "Ropsten",
+  //logoUrl: ethereumLogoUrl,
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Ropsten Ether", symbol: "ropETH", decimals: 18 },
+  //rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.ROPSTEN],
+  //},
+  //},
+  //[SupportedChainId.KOVAN]: {
+  //networkType: NetworkType.L1,
+  //docs: "https://docs.uniswap.org/",
+  //explorer: "https://kovan.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/",
+  //label: "Kovan",
+  //logoUrl: ethereumLogoUrl,
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Kovan Ether", symbol: "kovETH", decimals: 18 },
+  //rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.KOVAN],
+  //},
+  //},
+  //[SupportedChainId.GOERLI]: {
+  //networkType: NetworkType.L1,
+  //docs: "https://docs.uniswap.org/",
+  //explorer: "https://goerli.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/",
+  //label: "Görli",
+  //logoUrl: ethereumLogoUrl,
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Görli Ether", symbol: "görETH", decimals: 18 },
+  //rpcUrl: INFURA_NETWORK_URLS[SupportedChainId.GOERLI],
+  //},
+  //},
+  //[SupportedChainId.OPTIMISM]: {
+  //networkType: NetworkType.L2,
+  //blockWaitMsBeforeWarning: ms("25m"),
+  //bridge: "https://gateway.optimism.io/?chainId=1",
+  //defaultListUrl: OPTIMISM_LIST,
+  //docs: "https://optimism.io/",
+  //explorer: "https://optimistic.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/optimism/",
+  //label: "Optimism",
+  //logoUrl: optimismLogoUrl,
+  //statusPage: "https://optimism.io/status",
+  //helpCenterUrl:
+  //"https://help.uniswap.org/en/collections/3137778-uniswap-on-optimistic-ethereum-oξ",
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  //rpcUrl: "https://mainnet.optimism.io",
+  //},
+  //},
+  //[SupportedChainId.OPTIMISTIC_KOVAN]: {
+  //networkType: NetworkType.L2,
+  //blockWaitMsBeforeWarning: ms("25m"),
+  //bridge: "https://gateway.optimism.io/",
+  //defaultListUrl: OPTIMISM_LIST,
+  //docs: "https://optimism.io/",
+  //explorer: "https://optimistic.etherscan.io/",
+  //infoLink: "https://info.uniswap.org/#/optimism/",
+  //label: "Optimistic Kovan",
+  //logoUrl: optimismLogoUrl,
+  //statusPage: "https://optimism.io/status",
+  //helpCenterUrl:
+  //"https://help.uniswap.org/en/collections/3137778-uniswap-on-optimistic-ethereum-oξ",
+  //addNetworkInfo: {
+  //nativeCurrency: {
+  //name: "Optimistic Kovan Ether",
+  //symbol: "kovOpETH",
+  //decimals: 18,
+  //},
+  //rpcUrl: "https://kovan.optimism.io",
+  //},
+  //},
+  //[SupportedChainId.ARBITRUM_ONE]: {
+  //networkType: NetworkType.L2,
+  //blockWaitMsBeforeWarning: ms("10m"),
+  //bridge: "https://bridge.arbitrum.io/",
+  //docs: "https://offchainlabs.com/",
+  //explorer: "https://arbiscan.io/",
+  //infoLink: "https://info.uniswap.org/#/arbitrum",
+  //label: "Arbitrum",
+  //logoUrl: arbitrumLogoUrl,
+  //defaultListUrl: ARBITRUM_LIST,
+  //helpCenterUrl:
+  //"https://help.uniswap.org/en/collections/3137787-uniswap-on-arbitrum",
+  //addNetworkInfo: {
+  //nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  //rpcUrl: "https://arb1.arbitrum.io/rpc",
+  //},
+  //},
+  //[SupportedChainId.ARBITRUM_RINKEBY]: {
+  //networkType: NetworkType.L2,
+  //blockWaitMsBeforeWarning: ms("10m"),
+  //bridge: "https://bridge.arbitrum.io/",
+  //docs: "https://offchainlabs.com/",
+  //explorer: "https://rinkeby-explorer.arbitrum.io/",
+  //infoLink: "https://info.uniswap.org/#/arbitrum/",
+  //label: "Arbitrum Rinkeby",
+  //logoUrl: arbitrumLogoUrl,
+  //defaultListUrl: ARBITRUM_LIST,
+  //helpCenterUrl:
+  //"https://help.uniswap.org/en/collections/3137787-uniswap-on-arbitrum",
+  //addNetworkInfo: {
+  //nativeCurrency: {
+  //name: "Rinkeby Arbitrum Ether",
+  //symbol: "rinkArbETH",
+  //decimals: 18,
+  //},
+  //rpcUrl: "https://rinkeby.arbitrum.io/rpc",
+  //},
+  //},
   [SupportedChainId.POLYGON]: {
     networkType: NetworkType.L1,
     blockWaitMsBeforeWarning: ms("10m"),
